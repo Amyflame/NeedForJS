1088e09961be18e880418dd9c7321386b1687b7e
Amyflame <70804508+Amyflame@users.noreply.github.com>
2020-09-12 21:43:55 +0300

    День 3

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ const score = document.querySelector('.score'),
   
   function startGame() {
     start.classList.add('hide');
+    gameArea.innerHTML = "";
+
 
     for (let i = 0; i < getQuantityElements(100) ; i ++){
      const line = document.createElement('div');
@@ -49,8 +51,13 @@ const score = document.querySelector('.score'),
       enemy.style.background = 'transparent url("./image/enemy2.png") center / cover no-repeat';
       gameArea.appendChild(enemy);
     }
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = "auto";
+    car.style.bottom = "10px";
+
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -59,6 +66,8 @@ const score = document.querySelector('.score'),
   function playGame(){
      
       if (setting.start){
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE<br>' + setting.score;
         moveRoad();
         moveEnemy();
 
@@ -107,6 +116,19 @@ const score = document.querySelector('.score'),
   function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item) {
+      let carRect = car.getBoundingClientRect();
+      let enemyRect = item.getBoundingClientRect();
+      if (carRect.top <= enemyRect.bottom && 
+        carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right &&
+        carRect.bottom >= enemyRect.top){
+          setting.start = false;
+
+        console.warn('ДТП');
+        start.classList.remove('hide');
+        start.style.top = score.offsetHeight;
+      }
+    
       item.y += setting.speed / 2;
       item.style.top = item.y + 'px';
       if (item.y >= document.documentElement.clientHeight){
